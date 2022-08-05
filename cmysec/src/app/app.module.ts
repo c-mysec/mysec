88ebe3247cb4c4c5a5f4ac58af05108447b33483
Clovis Yutaka Harada <clovis.harada@gmail.com>
2022-08-05 08:06:22 -0300

    initial

---
+++ cmysec/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { SetupComponent } from './setup/setup.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+
+import { ConfigComponent } from './config/config.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SetupComponent,
+    ConfigComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cmysec/src/app/config/config.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+@Component({
+  selector: 'app-config',
+  templateUrl: './config.component.html',
+  styleUrls: ['./config.component.css']
+})
+export class ConfigComponent implements OnInit {
+
+  /*
+  Node Name
+ESP32
+Node User Name
+admin
+Node User Password
+12345678
+Iot Name
+clovis harada
+Iot Password
+lockheed
+Iot endpoint
+
+Iot out topic
+  */
+
+  configForm = this.formBuilder.group({
+    nodeName: '',
+    userName: '',
+    password: '',
+    iotName: '',
+    iotPassword: '',
+    iotEndpoint: '',
+    iotTopic: ''
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(): void {
+    console.warn('Your order has been submitted', this.configForm.value);
+    this.configForm.reset();
+  }
+  reset(): void {
+    console.warn('reset', this.configForm.value);
+    this.configForm.reset();
+  }
+}
